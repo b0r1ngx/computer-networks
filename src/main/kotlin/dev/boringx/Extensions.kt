23fa181c9087436b1f4bdce0a793ab6e5528c5c6
Ivanov Kirill A <ghztrikz1337@gmail.com>
2024-04-14 22:58:13 +0300

    Extract common sendResponse200

--- src/main/kotlin/dev/boringx/Extensions.kt
+++ src/main/kotlin/dev/boringx/Extensions.kt
@@ -2,8 +2,15 @@ package dev.boringx
 
 import dev.boringx.resources.values.FileNotFound
 import dev.boringx.resources.values.NOT_FOUND_404
+import dev.boringx.resources.values.OK_200
 import java.io.OutputStream
 import java.lang.Exception
+
+fun OutputStream.sendResponse200(data: ByteArray) {
+    write(OK_200.toByteArray())
+    write(data)
+    println("[TRACE] Application found route.")
+}
 
 fun OutputStream.sendResponse404(exception: Exception) {
     write(NOT_FOUND_404.toByteArray())
